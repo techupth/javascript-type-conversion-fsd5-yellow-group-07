47b05c9f7f855bef5275cafce964a5859a2bd62c
ณัฐพร นาคพงษ์ <Missterdonat33@gmail.com>
2023-11-23 13:28:45 +0700

    feat:EX.Type Conversion

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,5 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
-
-accountBalance = accountBalance + depositAmountFromUser;
-
-let accountBalanceMessage = "Your account balance is " + accountBalance;
-
+accountBalance = accountBalance + Number(depositAmountFromUser);
+let accountBalanceMessage = `Your account balance is ${accountBalance}`;
 console.log(accountBalanceMessage);
